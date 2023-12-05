74ba88026d6c53a1c5e057325b290ce46cb3455f
Markayel <mtorelus@gmail.com>
2023-12-05 11:39:02 -0800

    made edits, fixed indentation pt 2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,30 +3,30 @@ import { Nav, NavItem } from "reactstrap"
 import { NavLink } from "react-router-dom"
 
 const Header = () => {
-  return (
-<> 
-<h2> Cat Tinder!</h2>
-<header> 
-    <Nav> 
-        <NavItem>
-            <NavLink to="/"> 
-                Home
-            </NavLink> 
-        </NavItem>
-        <NavItem>
-            <NavLink to="/catindex">
-                Let's take a look!
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink to="/catnew">
-                Start today!
-            </NavLink>
-        </NavItem>
-    </Nav>
-</header>
-</>
-  )
+    return (
+        <>
+            <h2> Cat Tinder!</h2>
+            <header>
+                <Nav>
+                    <NavItem>
+                        <NavLink to="/">
+                            Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="/catindex">
+                            Let's take a look!
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="/catnew">
+                            Start today!
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </header>
+        </>
+    )
 }
 
 export default Header;
